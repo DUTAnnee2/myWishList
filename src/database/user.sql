bf94b1df9bd31eeb7ff01e29186fc7dce2a8da76
Eurêka <therealeureka@protonmail.com>
2021-12-22 17:02:44 +0100

    Added user.sql

---
+++ src/database/user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+                        `user_id` int(5) NOT NULL,
+                        `username` varchar(20) NOT NULL,
+                        `email` varchar(255) NOT NULL,
+                        `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
