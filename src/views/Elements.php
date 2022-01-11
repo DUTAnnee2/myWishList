4b94cc2e243c5dd586d006a145b7ccb0425c535a
shvde <72690981+shvde@users.noreply.github.com>
2022-01-11 13:55:21 +0100

    Update Elements.php

--- src/views/Elements.php
+++ src/views/Elements.php
@@ -2,83 +2,118 @@
 
 namespace mywishlist\views;
 
+
+/**
+ * Class Elements, used to render the differents reusable elements of the website
+ */
 class Elements
 {
 
-    public function renderHeaders() : string
+    /**
+     * Render the HTML headers
+     * @return string The HTML headers
+     */
+    public function renderHeaders(): string
     {
-        $html = <<<HTML
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/web/css/main.css">
-    <title>MyWishList</title>
-</head>
-<body>
-
+        return <<<HTML
+            <!doctype html>
+            <html lang="fr">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <link rel="stylesheet" href="/web/css/main.css">
+                    <title>MyWishList</title>
+                </head>
+            <body>
 HTML;
-
-        return $html;
     }
 
-    public function renderHeader() : string
+    /**
+     * Render the HTML page header
+     * @return string The HTML page header
+     */
+    public function renderHeader(): string
     {
         $user = "";
         if (isset($_SESSION["userid"])) {
-            $user=  "Bonjour, " . $_SESSION["username"]."!";
+            $user = "Bonjour, " . $_SESSION["username"] . "!";
         }
 
-        $html = <<<HTML
-    <header>
-        <nav class="container-large">
-            <h1>
-                <a href="/participant">
-                    <span>My</span><span class="text-purple">WishList</span>
-                </a>
-            </h1>
+        return <<<HTML
+            <header>
+                <nav class="container-large">
+                    <h1>
+                        <a href="/participant">
+                            <span>My</span><span class="text-purple">WishList</span>
+                        </a>
+                    </h1>
                     $user
-            <a href="/login">
-                    <img src="/web/icons/user.svg" alt="user icon" class="user-icon">
-            </a>
-        </nav>
-    </header>
-    <main>
+                    <a href="/login">
+                            <img src="/web/icons/user.svg" alt="user icon" class="user-icon">
+                    </a>
+                </nav>
+            </header>
+            <main>
 HTML;
-
-        return $html;
     }
 
-    public function renderFormId() : string
-    {
-        $html = <<<HTML
-        <div class="form-container">
-            <form action="" method="post" class="id-form">
-                <input type="text" placeholder="ID de votre liste" class="form-input" name="id">
-                <input type="submit" value="CHERCHER" class="form-submit">
-            </form>
-        </div>
-HTML;
 
-        return $html;
+    /**
+     * Render the HTML form to get the ID of a list
+     * @return string The HTML form to get the ID of a list
+     */
+    public function renderFormId(): string
+    {
+        return <<<HTML
+            <div class="form-container">
+                <form action="" method="post" class="id-form">
+                    <input type="text" placeholder="ID de votre liste" class="form-input" name="id">
+                    <input type="submit" value="CHERCHER" class="form-submit">
+                </form>
+            </div>
+HTML;
     }
 
-    public function renderFooter() : string
+    /**
+     * Render the HTML page footer
+     * @return string The HTML page footer
+     */
+    public function renderFooter(): string
     {
-        $html = <<<HTML
-      </main>
-      <footer>
-        <nav class="container-large">
-            <h1>
-                <span>My</span><span class="text-purple">WishList</span>
-            </h1>
-        </nav>
-    </footer>
-    </body>
-    </html>
+        return <<<HTML
+                </main>
+                    <footer>
+                        <nav class="container-large">
+                            <h1>
+                                <span>My</span><span class="text-purple">WishList</span>
+                            </h1>
+                        </nav>
+                    </footer>
+            </body>
+            </html>
 HTML;
-        return $html;
+    }
+
+    /**
+     * Render the HTML buttons to edit a list
+     * @param $id
+     * @return string The HTML buttons to edit a list
+     */
+    public function renderInterractionCardButtons($id): string
+    {
+        return <<<HTML
+            <div class="card-interraction-btns">
+                <a href="#" class="btn">
+                    <img src="/web/icons/edit.svg" alt="edit icon">
+                </a>
+                <a href="/delete-list/$id" class="btn">
+                    <img src="/web/icons/delete.svg" alt="delete icon">
+                </a>
+                <a href="#" class="btn">
+                    <img src="/web/icons/share.svg" alt="share icon">
+                </a>
+            </div>
+HTML;
     }
 }
