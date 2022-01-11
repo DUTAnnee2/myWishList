5ff4f532398dd90dbbd3ae09a7868faf90ec2bdc
Eurêka <therealeureka@protonmail.com>
2022-01-11 14:20:18 +0100

    Fix namespaces

--- src/controllers/ItemController.php
+++ src/controllers/ItemController.php
@@ -1,18 +1,8 @@
 <?php
-namespace mywishlist\controller;
+namespace mywishlist\controllers;
 
   use \mywishlist\models\Item as Item;
-  use \mywishlist\views\ItemView as ItemView;
 
   class ItemController{
-
-    public function DisplayItem($idItem)
-    {
-        $item = Item::where(['id' => $idItem])->first();
-        $vue = new ItemView();
-        $vue->render($item);
-    }
-
-    //je voulais faire createItem mais zzz
-
+      
   }
