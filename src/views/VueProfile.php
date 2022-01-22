1600094dc50b9147b207f7eb8ac90ead526886ab
Eureka <therealeureka@protonmail.com>
2022-01-22 20:10:07 +0100

    user profile display

--- src/views/VueProfile.php
+++ src/views/VueProfile.php
@@ -8,10 +8,26 @@ class VueProfile
     function render(): string
     {
         $id = $_SESSION["userid"];
-        $u = \mywishlist\models\User::where('user_id', "=", $id)->get();
+        $u = \mywishlist\models\User::where('user_id', "=", $id)->get()[0];
+        $username = $u["username"];
+        $email =  $u["email"];
         $elements = new Elements();
         $render = $elements->renderHtmlHeaders() . $elements->renderHeader();
-        //TODO
-        return $render;
+        $render .= <<<HTML
+<div class="card-container container-large">
+<div class="card">
+<h2>$username</h2>
+<h4>$email</h4>
+            <div class="card-interraction-btns">
+          
+            <a href="/login">Déconnection    </a>
+       
+</div>
+</div>         
+            </div>
+              
+HTML;
+
+        return $render.$elements->renderFooter();
     }
 }
