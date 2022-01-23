fd2d93d228108dc6a043112247820c8b15cf044f
Eureka <therealeureka@protonmail.com>
2022-01-23 11:13:13 +0100

    Fix orderBy

--- src/controllers/MessageController.php
+++ src/controllers/MessageController.php
@@ -7,7 +7,7 @@ class MessageController
     function getMessages($listId)
     {
         $vue = new \mywishlist\views\VueMessage();
-         $msgs =  \mywishlist\models\Message::where('liste_id',"=",$listId)->orderBy('liste_id','DESC')->get();
+         $msgs =  \mywishlist\models\Message::where('liste_id',"=",$listId)->orderBy('mess_id','DESC')->get();
 
         return $vue->renderMessages($msgs->toArray(), $listId);
     }
