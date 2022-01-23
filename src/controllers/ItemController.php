1f3048203aaec6a8cc849e55a5577142c1f25100
shvde <72690981+shvde@users.noreply.github.com>
2022-01-23 12:40:03 +0100

    Add doc ItemController

--- src/controllers/ItemController.php
+++ src/controllers/ItemController.php
@@ -6,61 +6,102 @@ use JetBrains\PhpStorm\NoReturn;
 use JetBrains\PhpStorm\Pure;
 use \mywishlist\models\Item as Item;
 
+/**
+ * Class ItemController
+ * @author 1shade
+ * @author Eureka
+ * @package mywishlist\controllers
+ */
 class ItemController
 {
 
-
-    function getItem($id): string
+    /**
+     * Get a specific item
+     * @param $id int Item id
+     * @return string Item view
+     */
+    function getItem(int $id): string
     {
-        $listl = \mywishlist\models\Item::find($id);
+        $listl = Item::find($id);
         $vue = new \mywishlist\views\VueListItem([$listl]);
-
         return $vue->renderLists(3);
     }
 
-    #[NoReturn] function deleteItem($listid,$id)
+
+    /**
+     * Delete an item in a list
+     * @param $listid int List id
+     * @param $id int Item id
+     * @return void Redirect to the modified list
+     */
+    #[NoReturn] function deleteItem(int $listid, int $id) : void
     {
-        $item = \mywishlist\models\Item::find($id);
+        $item = Item::find($id);
         $item->delete();
-        header('Location: /'.$listid);
+        header('Location: /' . $listid);
         exit();
     }
 
-    function editItem($id): string
+    /**
+     * Edit an item to a list
+     * @param $id int Item id
+     * @return string Item view
+     */
+    function editItem(int $id): string
     {
-        $item = \mywishlist\models\Item::find($id);
+        $item = Item::find($id);
         $vue = new \mywishlist\views\VueCreateEditItem();
         return $vue->renderModifier($id);
     }
 
-    #[NoReturn] public function saveEditedItem($listid, $id)
+    /**
+     * Update an item
+     * @param $listid int List id
+     * @param $id int Item id
+     * @return void Redirect to the modified list
+     */
+    #[NoReturn] public function saveEditedItem(int $listid, int $id) : void
     {
-        $item = \mywishlist\models\Item::find($id);
-        $item->nom = $_POST['titre'];
-        $item->descr = $_POST['description'];
-        $item->img = $_POST['img'];
-        $item->tarif = $_POST['price'];
-        $item->save();
-        header('Location: /'.$listid);
-        exit();
+        $item = Item::find($id);
+        $this->listInformations($item, $listid);
     }
 
+    /**
+     * Add an item to a list
+     * @return string Item view
+     */
     #[Pure] public function createNewItem(): string
     {
         $vue = new \mywishlist\views\VueCreateEditItem();
         return $vue->renderCreateItem();
     }
 
-    #[NoReturn] public function saveNewItem($listid)
+    /**
+     * Save an item
+     * @param $listid int List id
+     * @return void Redirect to the modified list
+     */
+    #[NoReturn] public function saveNewItem(int $listid)
     {
-        $item = new \mywishlist\models\Item;
+        $item = new Item;
         $item->liste_id = $listid;
+        $this->listInformations($item, $listid);
+    }
+
+    /**
+     * Get informations from the list
+     * @param $item Item Item
+     * @param int $listid List id
+     * @return void Redirect to the modified list
+     */
+    #[NoReturn] public function listInformations(Item $item, int $listid): void
+    {
         $item->nom = $_POST['titre'];
         $item->descr = $_POST['description'];
         $item->img = $_POST['img'];
         $item->tarif = $_POST['price'];
         $item->save();
-        header('Location: /'.$listid);
+        header('Location: /' . $listid);
         exit();
     }
 
